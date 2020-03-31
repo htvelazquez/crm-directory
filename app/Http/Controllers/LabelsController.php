a2917f438e894b7ada8a40135fcb47be5555ea3e
htvelazquez <htvelazquez@gmail.com>
2020-03-31 18:20:18 -0300

    Creation of contact account relationship Creation of contact_account label relationship New timestamps fields for contacts labels Some fixes on snapshots creation process

--- app/Http/Controllers/LabelsController.php
+++ app/Http/Controllers/LabelsController.php
@@ -3,14 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Label;
 
 class LabelsController extends Controller
 {
     public function get(Request $request) {
+        $accountId = 1; // get as parameter
 
-        $labels = [
-            "Recruiting","PHP Devs","Buenos Aires","Washington","Mexico City","Buenos Aires","Sydney","Wellington","Canberra","Beijing","New Delhi","Kathmandu","Cairo","Cape Town","Kinshasa"
-        ];
+        $dbLabels = Label::where('account_id',$accountId)->get();
+
+        if (empty($dbLabels)) return response()->json(['suggestions' => []], 200);
+
+        $labels = [];
+        foreach ($dbLabels as $dbLabel) {
+            $labels[] = $dbLabel->name;
+        }
 
         if ($request->has('term')){
             $labels = preg_grep("/.*{$request->input('term')}.*/i",$labels);
